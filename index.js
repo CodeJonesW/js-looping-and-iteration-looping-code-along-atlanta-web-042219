b900f487ec1794276423d6ce7b2697aa3176703b
Will Jones <willjones2010@gmail.com>
2019-06-03 08:04:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function wrapGifts(gifts) {
+  let i = 0; // the initialization moved OUTSIDE the body of the loop!
+  while (i < gifts.length) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    i++; // the iteration moves INSIDE the body of the loop!
+  }
+
+  return gifts;
+}
